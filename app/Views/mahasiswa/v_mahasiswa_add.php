bcbf0525316045d8583550637ba3740a08149d7e
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-04 18:10:40 +0700

    create mahasiswa add

---
+++ app/Views/mahasiswa/v_mahasiswa_add.php
@@ -0,0 +1,11 @@
+<h2>Tambah Mahasiswa</h2>
+<form method="post" action="/mahasiswa/add">
+    <label>NIM:</label>
+    <input type="text" name="nim" required><br>
+    <label>Nama:</label>
+    <input type="text" name="nama" required><br>
+    <label>Jurusan:</label>
+    <input type="text" name="jurusan" required><br>
+    <button type="submit">Simpan</button>
+</form>
+<a href="/mahasiswa">Kembali</a>
