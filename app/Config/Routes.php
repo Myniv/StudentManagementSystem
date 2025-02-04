4bb0d43096a44566da7bf6f3265cb1f4d2d6c043
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-04 17:02:17 +0700

    Update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Controllers\MahasiswaController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/mahasiswa', [MahasiswaController::class, "index"]);
+$routes->get('/mahasiswa/detail/(:num)', [MahasiswaController::class, "detail"]);
