82c0b9718d1504e6b180fc35b512ae59c87f0bc5
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-04 19:07:31 +0700

    Update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,8 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/mahasiswa', [MahasiswaController::class, "index"]);
 $routes->get('/mahasiswa/detail/(:num)', [MahasiswaController::class, "detail"]);
+$routes->get('/mahasiswa/create', [MahasiswaController::class, "goCreate"]);
+$routes->post('/mahasiswa/add', [MahasiswaController::class, "create"]);
+$routes->delete('/mahasiswa/delete/(:num)', [MahasiswaController::class, "delete"]);
+$routes->get('/mahasiswa/update/(:num)', [MahasiswaController::class, "goUpdate"]);
+$routes->post('/mahasiswa/update', [MahasiswaController::class, "update"]);
